7083b05c3b661de159e3d211e5573a4fb55b5803
ju1c3007 <anaavalo.ribeiro@gmail.com>
2024-11-14 19:54:11 -0400

    first commit

---
+++ src/i18n/ptBR.ts
@@ -0,0 +1,17 @@
+export const ptBR = {
+    translation:{
+        home: {
+            searchButton:"Buscar",
+            resultsFoundMessage: "{{numberOfResults}} resultado(s) encontrado(s)",
+            noResultsFoundMessage: "Nenhuma palavra encontrada",
+            wordDetails: "{{numberOfMeanings}} significado(s) e {{numerOfAudios}} áudios de pronúncia",
+        },
+        details: {
+            detailsTitle: "Significados de {{word}}",
+            meaningsPanelTitle: "Significados",
+            audiosPanelTitle: "Áudios de pronúncia",
+            backButton: "Voltar",
+        },
+    },
+
+}
